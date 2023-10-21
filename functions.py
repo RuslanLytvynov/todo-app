8eab389faf026bf14480b281f92366bfd3aa48a4
RuslanLytvynov <lytvynovrus@gmail.com>
2023-10-21 19:16:46 +0200

    add simple gui without functional

---
+++ functions.py
@@ -0,0 +1,20 @@
+FILEPATH = 'todos.txt'
+
+def get_todos (filepath=FILEPATH):
+    """Reads a text file and returns a list of todos items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos (todos_arg, filepath=FILEPATH):
+    """Writes the todos items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+
+
+if __name__ == "main":
+    print("Hello")
+    print(get_todos())
